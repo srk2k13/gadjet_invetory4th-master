c7cf65241094c8f6ed1697fd7527ce88f7921220
srk2k19 <svn_k1@yahoo.co.in>
2021-02-10 14:08:50 +0530

    Add files via upload

---
+++ lib/Data.dart
@@ -0,0 +1,5 @@
+class Data {
+  String devicetype, imgUrl,wcimgUrl,ugimgUrl, wcpdfUrl,ugpdfUrl,cattegorrytype,companyname,modelname,seriesname,year,serielnumber,warrantydate,name,mobilenumber,address,website, uploadid;
+
+  Data(this.devicetype,this.imgUrl ,this.wcimgUrl,this.ugimgUrl,this.wcpdfUrl,this.ugpdfUrl,this.cattegorrytype,this.companyname,this.modelname, this.seriesname,this.year,this.serielnumber,this.warrantydate,this.name,this.mobilenumber,this.address,this.website,this.uploadid);
+}
